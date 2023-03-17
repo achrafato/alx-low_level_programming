f23bfaf9d6636d0ac9360ba04e5a1844b714c15b
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-17 15:20:33 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,23 @@
 */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
-	for (i = 0; i <= 9 ; i++){
+	for (i = 0; i <= 9 ; i++)
+	{
 
-		for (j = i + 1 ; j <= 9; j++){
+		for (j = i + 1 ; j <= 9; j++)
+		{
 
-			putchar(i+'0');
-			putchar(j+'0');
+			putchar(i + '0');
+			putchar(j + '0');
 
 			if (j == 9 && i == 8)
 				continue;
 
 			putchar(',');
 			putchar(' ');
-		} 
+		}
 	}
 
 	putchar('\n');
