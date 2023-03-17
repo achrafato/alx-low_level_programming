514a3d075c4f844e1e5cfd3e9f386a1d90c74ad6
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-17 11:54:14 -0700

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - random number
+* main - prints all single digit numbers of base 10 starting from 0
 *
 * Return: Always 0.
 */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i=0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 		printf("%d", i);
 
 	printf("\n");
