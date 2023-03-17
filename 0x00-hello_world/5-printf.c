ac59761debdd1812aa1e56e841b6e0c21475128a
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-16 20:08:49 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
-int main(void){ 
+#include <stdio.h>
 
-        printf("with proper grammar, but the outcome is a piece of art,\n"); 
-        return (0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
