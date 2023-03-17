64c81e45a256129d18a8fe516a12d3d42fb88d62
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-17 10:32:49 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: achraf ibn cheikh
+ */
+
+#include <stdio.h>
+
+ /**
+  * main - Prints the alphabet in lowercase except 'q' and 'e'.
+  *
+  * Return: Always 0.
+  */
+int main(void) {
+
+  char letter;
+
+  for (letter = 'a'; letter <= 'z'; letter++) {
+      
+      if(letter != 'q' && letter != 'e'){
+          putchar(letter);
+      }
+    
+  }
+
+  putchar('\n');
+
+  return (0);
+}
