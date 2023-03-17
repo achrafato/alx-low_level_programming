034c0caf2a38e7ad46a2425d65e517e4e175a065
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-17 09:57:32 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,17 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: achraf ibn cheikh
+ */
+
 #include <stdio.h>
 
-
 int main(void) {
+ 
+ /**
+  * main - Prints the alphabet in lowercase.
+  *
+  * Return: Always 0.
+  */
   char letter;
 
   for (letter = 'a'; letter <= 'z'; letter++) {
