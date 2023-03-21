30f98ee677827d85384a64d8cfdd80b3c927b525
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 10:25:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	int i;
+	char c[8] = "_putchar";
+	
+	for(i = 0; i < 8; i++ )
+	{
+		_putchar(c[i]);
+	}
 
-return (0);
+	_putchar('\n');
+
+	return (0);
 }
