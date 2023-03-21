bd1c762cad48f6b9eb45103d552992340d2edca4
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 12:44:02 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet 10 times
+*
+* Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+_putchar('\n')
+}
+}
