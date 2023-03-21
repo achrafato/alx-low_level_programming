d04da33828fc117df22520addd2d1780e2897af6
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 14:43:54 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+* print_sign - Check if number is positive or negative
+* @n: number to be verified
+* Return: return 1 or 0 or -1.
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
