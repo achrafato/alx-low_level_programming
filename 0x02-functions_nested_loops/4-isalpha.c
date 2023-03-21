d96280163f37c2d6ca99cf2338c2fa8b7d7dba42
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 14:30:57 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+/**
+* _isalpha - Check if the char is alphabetic or not
+* @c: character to be verified
+* Return: return 1 or 0.
+*/
+
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
