cbbba4e0dd89de76a373367769f4d5276e220169
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 14:56:28 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - measure the absolute value
+* @n: number to be calculated
+* Return: return the absolute value of a number.
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+_putchar(n);
+}
+else if (n < 0)
+{
+_putchar(-n);
+}
+}
