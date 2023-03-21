9f5dd70ffb9ce666d0ad5cd1ac3cfb3d4ab87756
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 12:20:14 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,12 @@
 #include "main.h"
 
 /**
-* main - prints the alphabet
+* print_alphabet - prints the alphabet
 *
 * Return: Always 0.
 */
-
-int main()
+void print_alphabet(void)
 {
-print_alphabet();
-return 0;
-}
-
-
-void print_alphabet(void){
 int i;
 char alphat[] = "abcdefghijklmnopqrstuvwxyz";
 for (i = 0; i < 26; i++)
