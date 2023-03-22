277d44fb749a1d5e28022f3676852150de7727ce
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 19:48:38 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+* add - sum two numbers
+* @a: first number
+* @b: second number
+* Return: Returns the sum of the given numbers
+*/
+
+int add(int a, int b)
+{
+int res = a + b;
+
+return (res);
+}
