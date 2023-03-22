b14482bf343d218ae567f8f25c472a237815c73a
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-21 19:41:40 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,20 +12,20 @@ void times_table(void)
 int digit, mult, result;
 for (digit = 0; digit < 10; digit++)
 {
-putchar('0');
+_putchar('0');
 
 for (mult = 1; mult < 10; mult++)
 {
-putchar(',');
-putchar(' ');
+_putchar(',');
+_putchar(' ');
 result = digit * mult;
 if (result <= 9)
-putchar(' ');
+_putchar(' ');
 else
-putchar((result / 10) + '0');
+_putchar((result / 10) + '0');
 
-putchar((result % 10) + '0');
+_putchar((result % 10) + '0');
 }
-putchar(10);
+_putchar(10);
 }
 }
