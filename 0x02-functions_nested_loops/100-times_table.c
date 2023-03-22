ddbdd20a72b600dc3c875b2449a512fadcdf764e
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-22 08:41:18 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* print_times_table - prints the n times table
+* @n: n denotes the last number
+* Return: .
+*/
+
+void print_times_table(int n)
+{
+if (n > 15 || n < 0)
+{
+return (0);
+}
+
+int i,j;
+
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+printf("%d",(i * j));
+if (j != n)
+{
+if ((i * j) <= 9)
+{
+printf(",  ");
+}
+else if ((i * j) >= 100)
+{
+printf(",");
+}
+else
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+} 
+}
