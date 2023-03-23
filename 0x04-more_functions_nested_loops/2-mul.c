7db9c0dff78c1d694c07dab650cdd08d59472847
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-23 09:39:11 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiply two numbers
+ * @a: the number to be calculated
+ * @b: the number to be calculated
+ * Return: the multiple of two integers.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
