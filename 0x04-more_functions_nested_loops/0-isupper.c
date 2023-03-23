bed9f17f2eda2fb76c2989165ae347e7722528bc
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-23 08:50:26 -0700

    isUpper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,17 @@
 
-
+/**
+ * _isupper - tells if char is uppercase or not.
+ * @c: the char to be validate
+ * Return: 0 or 1.
+ */
 
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
 return (1);
-}else
+}
+else
 {
 return (0);
 }
