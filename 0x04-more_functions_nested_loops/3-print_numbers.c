b0d9874e26f3be354e8868520c7c06ab495876b0
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-23 09:57:52 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ *
+ * Return: the multiple of two integers.
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
