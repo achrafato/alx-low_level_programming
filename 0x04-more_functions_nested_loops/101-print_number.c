4a3f8fba5776407c0507088b3786cac9208be6ae
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-24 15:33:24 -0700

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+/**
+ * print_number - prints an integer
+ *@n: number to be displayed
+ * Return: 0.
+ */
+
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+if (n >= 1000)
+{
+int f1 = n % 1000;
+int f2 = f1 % 100;
+_putchar((n / 1000) + '0');
+_putchar((f1 / 100) + '0');
+_putchar((f2 / 10) + '0');
+_putchar((f2 % 10) + '0');
+}
+else if (n >= 100)
+{
+int three = n % 100;
+_putchar((n / 100) + '0');
+_putchar((three / 10) + '0');
+_putchar((three % 10) + '0');
+}
+else if (n >= 10)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if (n <= 9)
+{
+_putchar((n % 10) + '0');
+}
+}
