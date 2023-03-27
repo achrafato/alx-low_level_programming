5d80d6559e25fa0f334c58554cbbbcf322a2bf68
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-27 09:18:37 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 /**
- * main - check the code 
- *
+ * reset_to_98 - this function allow us to update a variable,
+ * by passing it own address
+ *@n: variable that will point on our var
  * Return: Always 0.
  */
- 
+
 void reset_to_98(int *n)
 {
 	*n = 98;
