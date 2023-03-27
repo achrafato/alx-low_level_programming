f8d76625ce86c8c4c66a78fdd31d4636b2bcc516
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-27 15:24:44 -0700

    5. A good engineer thinks in reverse and asks ..

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+/**
+ * rev_string - swap numbers
+ *@s: char that pointing to the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+int i, j;
+j = 0;
+char newS[5];
+for (i = strlen(s) - 1; i >= 0; i--)
+{
+newS[j] = s[i];
+j++;
+}
+
+i = 0;
+while (i <= strlen(s) - 1)
+{
+s[i] = newS[i];
+i++;
+}
+}
