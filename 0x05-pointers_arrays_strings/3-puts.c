90aa5a16cc9423e9fd65a51d5d61d5ccd04d0b52
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-27 11:25:28 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * _puts - prints a string
+ *@str: pointing to the string that will be printed
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		*str++;
+	}
+}
