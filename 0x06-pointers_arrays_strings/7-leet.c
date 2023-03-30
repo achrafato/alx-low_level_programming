de3cfa0c00172839741c1c86f8d5b2243482dd5f
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-30 09:41:57 -0700

    7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include <string.h>
+/**
+ * leet -  encodes a string into 1337
+ * @str: pointer to string
+ * Return: the address of my string.
+ */
+
+char *leet(char *str)
+{
+int i, j, size = strlen(str);
+
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (str[i] == s1[j])
+{
+str[i] = s2[j];
+}
+}
+}
+return (str);
+}
