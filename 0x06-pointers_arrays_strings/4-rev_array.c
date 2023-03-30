767cf69a7f89c5e76a5dd1bfd860c46ad55a9c2c
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-29 19:58:30 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+/**
+* reverse_array - reverses the content of an array of integers.
+*@n: number of elements of the array
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, crat;
+for (i = 0; i < n / 2 ; i++)
+{
+crat = a[i];
+a[i] = a[(n - 1) - i];
+a[(n - 1) - i] = crat;
+}
+}
