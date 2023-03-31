87e3c1fa0339ac971edb1f52dc510bcbdc39bf47
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-03-30 18:36:04 -0700

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,28 +3,29 @@
  * print_number - prints an integer
  * @n: integer to be print
  */
-
+ 
 void print_number(int n)
 {
-unsigned int num;
-int last, first, crat = 0;
-if (n == 0)
+int last, first, crat = 0;   
+unsigned int num = n;
+
+if (num == 0)
 {
 _putchar('0');
 }
 if (n < 0)
 {
 _putchar('-');
-num = -n;
+num = -n; 
 }
 
-while (num > 0)
+while(num > 0)
 {
 last = num % 10;
 crat = crat * 10 + last;
 num /= 10;
 }
-while (crat > 0)
+while(crat > 0)
 {
 first = crat % 10;
 _putchar(first + '0');
