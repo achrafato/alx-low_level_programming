4ee2f5b2dbcebb6846a07eebbb937a1e9355f3a2
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-03 10:18:07 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
-#include <stddef.h>
 /**
  * _strchr - locates a character in a string
  *@s: pointer to char
  *@c: the character to be searched for
  * Return: the address of char.
  */
+
 char *_strchr(char *s, char c)
 {
 while (*s != '\0')
@@ -15,5 +15,5 @@ return (s);
 }
 s++;
 }
-return (NULL);
+return (0);
 }
