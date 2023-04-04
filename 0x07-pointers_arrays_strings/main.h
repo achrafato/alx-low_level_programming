e788163264fafbf548ce3d0d08f06e029841149a
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-04 09:42:51 -0700

    100-set_string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 unsigned int _strspn(char *s, char *accept);
+void set_string(char **s, char *to);
 #endif
