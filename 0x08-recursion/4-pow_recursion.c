7561302f2e15db31765a079ba4739c16a6028886
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-05 10:45:36 -0700

    pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+
+/**
+ * _pow_recursion - calculate x raised to the power of y
+ * @x : number to be powered
+ * @y: multiplying x by itself y times
+ * Return: num or -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+
+if (y == 0)
+return (1);
+
+return (x * _pow_recursion(x, y - 1));
+}
