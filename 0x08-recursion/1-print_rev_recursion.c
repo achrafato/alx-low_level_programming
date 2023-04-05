1d104dbea036f173299c36317e88ef9370262109
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-05 09:14:12 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,12 +9,12 @@
 void _print_rev_recursion(char *s)
 {
 
-if (*s == '\n')
+if (*s == '\n' && strlen(s) > 0)
 {
 s += strlen(s) - 1;
 }
 
-if (*s != '\n')
+if (*s != '\n' && strlen(s) > 0)
 {
 _putchar(*s);
 s--;
