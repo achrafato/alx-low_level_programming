b18408a90b87a0d9714b79bd61d921ac34b191ba
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-07 08:53:34 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argv: An array containing the program command line arguments
+ * @argc: contain the length of command line arguments
+ * Return: 0 or 1.
+ */
+
+int main(int argc, char *argv[])
+{
+
+int i;
+int sum = 0;
+
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+char *str = argv[i];
+if (*str >= 48 && *str <= 57)
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("%d\n", sum);
+}
+else
+{
+printf("%d\n", sum);
+}
+
+return (0);
+}
