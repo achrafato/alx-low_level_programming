39e98e7ec677e6577e54579dc03b4802e0b21d2d
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-17 11:03:22 -0700

    print name of the current file

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - print the current file name
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
