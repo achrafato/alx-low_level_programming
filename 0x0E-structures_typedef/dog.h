429051ae8c9ae5afac12367d70489ffbb56ec755
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-17 17:46:33 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,11 @@
  * @owner: the owner of dog
  * Description: contains all the infos about dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
 #endif
