d9dddee45e3b5945a968cab375d5efbad8f03531
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-27 17:13:46 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
 #include "lists.h"
-#include <stddef.h>
-
 /**
- * list_len - loop through linkedList to the number of nodes
+ * list_len - loop through linkedList and return the number of nodes
  * @h: pointer to struct which to be printed
  * Return: number of nodes.
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+size_t n = 0;
 
-	while (h != NULL)
-	{
-		h = h->next;
-		n++;
-	}
-	return (n);
+while (h != NULL)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
