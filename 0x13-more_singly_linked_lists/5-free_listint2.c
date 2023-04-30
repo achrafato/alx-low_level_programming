65a30e17a64e9c0146fba5df37fba520576061b3
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-30 11:53:39 -0700

    free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list and assign head to null
+ * @head: pointer to struct
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *h;
+
+	h = *head;
+	while (h)
+	{
+		current = h->next;
+		free(h);
+		h = current;
+	}
+
+	*head = NULL;
+}
