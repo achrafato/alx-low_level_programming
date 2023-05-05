408d882981fa97620d7ab06642fd7fc9193e2836
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-04 18:09:51 -0700

    get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (index > (sizeof(n) * 8))
-		return (-1);
 	while (n)
 	{
 		if (index == i)
-			return (n % 2);
+		{
+			if (n % 2)
+				return (1);
+			else
+				return (0);
+		}
 		n /= 2;
 		i++;
 	}
