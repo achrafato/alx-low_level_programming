ab1158a6678c02be5fdc01f079ed77a2c36cc3fb
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-08 16:18:32 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
+		close(fc);
 		return (1);
-
+	}
 	fw = write(fc, text_content, strlen(text_content));
 	if (fw == -1)
 		return (-1);
