baa08e28703cde0e78eec18144a0c7f8d5134662
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-08 15:26:42 -0700

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file.
+ * @filename: pointer to char
+ * @text_content:  text to be written
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fc, fw;
+
+	if (filename == NULL)
+		return (-1);
+
+	fc = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
+
+	if (fc == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	fw = write(1, text_content, strlen(text_content));
+	if (fw == -1)
+		return (-1);
+
+	return (1);
+
+}
