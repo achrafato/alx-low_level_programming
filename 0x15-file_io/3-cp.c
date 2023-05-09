3706ded53be1b78b2c08372a82c2d4a5384ab88d
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-08 18:47:06 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,44 +11,44 @@
 int main(int ac, char **av)
 {
 int fo1, fo2, fr, fw;
-char *buffer;
+char buffer[1024];
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fo2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fo2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fo2 == -1)
 	{
-		close(fo2);
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
-	fo1 = open(av[1], O_RDWR);
+	fo1 = open(av[1], O_RDONLY);
 	if (fo1 == -1)
 	{
-		close(fo1);
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		return (98);
 	}
-	buffer = malloc(1024);
-	if (buffer == NULL)
-		return (0);
 	fr = read(fo1, buffer, 1024);
 	if (fr == -1)
-		exit(99);
-	fw = write(fo2, buffer, 1024);
-	if (fw == -1 || fw != 1024)
 	{
-		close(fo1);
-		close(fo2);
+		dprintf(2, "Error: Can't write to %s\n", av[1]);
+		exit(98);
+	}
+	fw = write(fo2, buffer, 1024);
+	if (fw == -1 || fw != fr)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	if (close(fo2) == -1 || close(fo1) == -1)
+	if (close(fo2) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fo2);
+		exit(100);
+	}
+	if (close(fo1) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fo1);
 		exit(100);
 	}
 	return (0);
