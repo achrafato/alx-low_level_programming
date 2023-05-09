49c420c89b8c640ce0c71432bd3e01bbf18eb84d
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-09 11:21:33 -0700

    copy the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,12 +14,14 @@ void check_status(int fo1, int fo2, char *av[])
 {
 	if (fo1 == -1)
 	{
+		close(fo1);
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
 	if (fo2 == -1)
 	{
+		close(fo2);
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
