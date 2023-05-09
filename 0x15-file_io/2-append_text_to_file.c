67b9989cc1005dd8e420b6dea6e8ebf1521b0c49
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-08 17:07:43 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,7 +33,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			i++;
 	}
 
-	fa  = write (fo, text_content, i);
+	fa  = write(fo, text_content, i);
 
 	if (fa == -1 || fa != i)
 	{
