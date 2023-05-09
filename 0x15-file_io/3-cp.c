bd50582042ca2ec74d6221c30104f38934ca25e6
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-09 10:02:59 -0700

    copy the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,12 +46,12 @@ char buffer[1024];
 
 	while ((fr = read(fo1, buffer, 1024)) > 0)
 	{
+		if (fr == -1)
+			check_status(fr, 0, av); 
 
 		fw = write(fo2, buffer, fr);
 		if (fw == -1 || fr != fw)
 			check_status(0, fw, av);
-		if (fr == -1)
-			check_status(fr, 0, av);
 	}
 
 	if (close(fo2) == -1)
