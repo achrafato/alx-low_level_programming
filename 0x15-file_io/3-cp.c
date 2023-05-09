7c04aa57735cf3542f903299cf3ca9b0c588344c
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-05-08 19:38:20 -0700

    copy the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,18 +43,14 @@ char buffer[1024];
 	fo2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	fo1 = open(av[1], O_RDONLY);
 	check_status(fo1, fo2, av);
+
 	fr = read(fo1, buffer, 1024);
 	if (fr == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		check_status(fr, 0, av);
+
 	fw = write(fo2, buffer, 1024);
-	if (fw == -1 || fw != fr)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+	if (fw == -1)
+		check_status(0, fw, av);
 	if (close(fo2) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fo2);
